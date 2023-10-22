92e6da4a69db88bf056a6415baf86e181ccabf25
Niranjan Kumawat <nkumawat@andrew.cmu.edu>
2023-10-21 20:51:53 -0400

    Add batch insertion to improve performance

--- spark/connectors/sink/database.py
+++ spark/connectors/sink/database.py
@@ -5,16 +5,23 @@ class PostgreSQLWriter(Writer):
         super().__init__()
 
     def __write_to_db(self, dataframe, epoch_id):
+        # TODO: .option("onConflict", "id") # For upsert behavior
+        # Check batch inserts to reduce connections. 
+        # .option("batchsize", "1000") \
+        # .option("rewriteBatchedInserts", "true") \
         return dataframe.write.format("jdbc") \
         .option("url", "jdbc:postgresql://localhost:5432/postgresDB") \
         .option("dbtable", "data") \
         .option("user", "postgres") \
         .option("password", "postgres") \
         .option("driver", "org.postgresql.Driver") \
+        .option("batchsize", "1000") \
+        .option("rewriteBatchedInserts", "true") \
         .mode("append") \
         .save()
 
     def writeStream(self, dataframe):
+        # TODO: Need to check the checkpoint for exactly once processing
         return dataframe.writeStream\
         .foreachBatch(self.__write_to_db)\
         .start()
